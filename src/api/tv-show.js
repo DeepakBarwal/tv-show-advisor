c8462092c866e7ee41a43b55f5a83ec533412b73
DeepakBarwal <deepakbarwal98@gmail.com>
2023-05-12 17:33:38 +0530

    Did some cleanup

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -4,48 +4,36 @@ import { BASE_URL, API_TOKEN } from "../constants";
 
 export class TVShowAPI {
   static async fetchPopular() {
-    try {
-      // perform the request
-      const response = await axios.get(`${BASE_URL}tv/popular`, {
-        headers: {
-          Authorization: `Bearer ${API_TOKEN}`,
-        },
-      });
-      // return the response
-      return response.data.results;
-      // return FAKE_POPULARS;
-    } catch (error) {
-      throw error;
-    }
+    // perform the request
+    const response = await axios.get(`${BASE_URL}tv/popular`, {
+      headers: {
+        Authorization: `Bearer ${API_TOKEN}`,
+      },
+    });
+    // return the response
+    return response.data.results;
+    // return FAKE_POPULARS;
   }
 
   static async fetchRecommendations(tvShowId) {
-    try {
-      const response = await axios.get(
-        `${BASE_URL}tv/${tvShowId}/recommendations`,
-        {
-          headers: {
-            Authorization: `Bearer ${API_TOKEN}`,
-          },
-        }
-      );
-      return response.data.results;
-      // return FAKE_RECOMMENDATIONS;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  static async fetchByTitle(title) {
-    try {
-      const response = await axios.get(`${BASE_URL}search/tv?query=${title}`, {
+    const response = await axios.get(
+      `${BASE_URL}tv/${tvShowId}/recommendations`,
+      {
         headers: {
           Authorization: `Bearer ${API_TOKEN}`,
         },
-      });
-      return response.data.results;
-    } catch (error) {
-      throw error;
-    }
+      }
+    );
+    return response.data.results;
+    // return FAKE_RECOMMENDATIONS;
+  }
+
+  static async fetchByTitle(title) {
+    const response = await axios.get(`${BASE_URL}search/tv?query=${title}`, {
+      headers: {
+        Authorization: `Bearer ${API_TOKEN}`,
+      },
+    });
+    return response.data.results;
   }
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import s from "./style.module.css";
 import { Search as SearchIcon } from "react-bootstrap-icons";
 
 const SearchBar = ({ onSubmit }) => {
+  const [value, setValue] = useState("");
+
   function submit(e) {
     if (e.key === "Enter" && e.target.value.trim() !== "") {
       onSubmit(e.target.value.trim());
+      setValue("");
     }
+  }
+
+  function handleChange(e) {
+    setValue(e.target.value);
   }
 
   return (
@@ -15,7 +23,9 @@ const SearchBar = ({ onSubmit }) => {
         className={s.input}
         type="text"
         placeholder="Search a TV show you may like"
+        onChange={handleChange}
         onKeyUp={submit}
+        value={value}
       />
     </>
   );
